2ac694b6d0ac26f5251c784742be13516d2cf215
Jesper Hansson Falkenby <jesper.hansson.f@gmail.com>
2017-06-11 03:55:20 +0200

    Update project to Bootstrap v4.0.0-alpha.6. Also updated Freelancer theme to a newer version based on Bootstrap 4. Résumé is now an image carousel. Fixed coloring scheme to blue. Plus some other tweaks.

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+$('.carousel-control.left').click(function() {
+  $('#resumeCarousel').carousel('prev');
+});
+
+$('.carousel-control.right').click(function() {
+  $('#resumeCarousel').carousel('next');
+});
